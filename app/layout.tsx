35456958cb3ab7cfdba10177c1b120689674ce7e
jsstoni <jsstoniha@gmail.com>
2025-09-30 22:19:18 -0300

    feat(ui): add sonner component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/provider";
+import { Toaster } from "@/components/ui/sonner";
 import { connection } from "next/server";
 
 const geistSans = Geist({
@@ -38,6 +39,7 @@ export default async function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
