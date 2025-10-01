21aa1b2f726222c678fae07907f36a1aa390ed8d
jsstoni <jsstoniha@gmail.com>
2025-09-30 23:46:22 -0300

    feat(ui): add profile component

---
+++ app/dashboard/_components/profile.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Skeleton } from "@/components/ui/skeleton";
+import { getInitials } from "@/lib/utils";
+import { authClient } from "@/utils/auth-client";
+import { LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+export function Profile() {
+  const router = useRouter();
+  const { data: session, isPending } = authClient.useSession();
+
+  const logout = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/signin");
+        },
+      },
+    });
+  };
+
+  if (isPending) {
+    return <Skeleton className="size-8 rounded-full" />;
+  }
+
+  if (!session) return null;
+
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Avatar>
+          <AvatarFallback>{getInitials(session.user.name)}</AvatarFallback>
+        </Avatar>
+      </PopoverTrigger>
+      <PopoverContent>
+        <p>{session.user.name}</p>
+        <p className="text-muted-foreground">{session.user.email}</p>
+        <Button size={"sm"} onClick={logout}>
+          <LogOut /> Logout
+        </Button>
+      </PopoverContent>
+    </Popover>
+  );
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getInitials(name: string): string {
+  const words = name.trim().split(/\s+/);
+  if (words.length === 1) {
+    return words[0].substring(0, 2).toUpperCase();
+  }
+  return (words[0][0] + words[1][0]).toUpperCase();
+}
