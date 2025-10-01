d30ea986c0145717feed958a6690b222fc1ff38b
jsstoni <jsstoniha@gmail.com>
2025-09-30 22:48:54 -0300

    feat(app): require user

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,7 @@
+import { requireUser } from "@/utils/require-user";
+
 export default async function Dashboard() {
+  await requireUser();
+
   return <p>Dashboard</p>;
 }
---
+++ utils/require-user.ts
@@ -0,0 +1,21 @@
+import "server-only";
+import { auth } from "@/utils/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { cache } from "react";
+
+export const getSession = cache(async () => {
+  return await auth.api.getSession({ headers: await headers() });
+});
+
+export const requireUser = cache(async () => {
+  const session = await getSession();
+
+  if (!session) {
+    redirect("/");
+  }
+
+  const { user } = session;
+
+  return user;
+});
