a2a74a2a153498cbe248ac4dd262395dda80ef67
jsstoni <jsstoniha@gmail.com>
2025-09-30 22:35:29 -0300

    feat(app): add signup and signin and dashboard page

---
+++ app/signin/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import Card from "@/components/card";
+import { Field } from "@/components/field";
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { userSchema } from "@/types/user";
+import { authClient } from "@/utils/auth-client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+export default function SignIn() {
+  const form = useForm({
+    resolver: zodResolver(userSchema),
+    defaultValues: { email: "", password: "" },
+  });
+
+  const onSubmit = form.handleSubmit(async (data) => {
+    const { error } = await authClient.signIn.email({
+      email: data.email,
+      password: data.password,
+      callbackURL: "/dashboard",
+    });
+
+    if (error) {
+      return toast.error(error.message);
+    }
+  });
+
+  return (
+    <main className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted">
+      <Card
+        title="Login to your account"
+        description="Enter your email below to login to your account"
+        className="min-w-sm"
+      >
+        <Form {...form}>
+          <form className="space-y-4" onSubmit={onSubmit}>
+            <Field
+              control={form.control}
+              name="email"
+              label="Email"
+              render={(field) => <Input type="email" {...field} />}
+            />
+            <Field
+              control={form.control}
+              name="password"
+              label="Password"
+              render={(field) => <Input type="password" {...field} />}
+            />
+            <Button size="sm">Login</Button>
+          </form>
+        </Form>
+      </Card>
+    </main>
+  );
+}
---
+++ types/user.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+
+export const userSchema = z.object({
+  email: z.email("Invalid email address").nonempty("Email is required"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
+
+export const registerSchema = userSchema
+  .extend({
+    name: z.string().nonempty("Name is required"),
+    repassword: z
+      .string()
+      .min(6, "Password must be at least 6 characters long"),
+  })
+  .refine((data) => data.password === data.repassword, {
+    path: ["repassword"],
+    message: "Passwords do not match",
+  });
