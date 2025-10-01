2f23b846136bb9f6f8a345e6c7d37a66733f42ce
jsstoni <jsstoniha@gmail.com>
2025-09-30 21:57:45 -0300

    feat(ui): add wrapper card component

---
+++ components/card.tsx
@@ -0,0 +1,30 @@
+import {
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  Card as CardUI,
+} from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+
+type CardProps = {
+  title: string;
+  description?: string;
+  children?: Readonly<React.ReactNode>;
+};
+
+export default function Card({
+  children,
+  className,
+  ...props
+}: CardProps & React.ComponentProps<"div">) {
+  return (
+    <CardUI className={cn(className)} {...props}>
+      <CardHeader>
+        <CardTitle>{props.title}</CardTitle>
+        <CardDescription>{props.description}</CardDescription>
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+    </CardUI>
+  );
+}
