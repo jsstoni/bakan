5d9c7113c4493977406508d990a9ca2c63f4126b
jsstoni <jsstoniha@gmail.com>
2025-10-04 18:20:00 -0300

    feat(ui): password component

---
+++ components/password.tsx
@@ -0,0 +1,29 @@
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+} from '@/components/ui/input-group';
+import { EyeClosedIcon, EyeIcon, LockIcon } from 'lucide-react';
+import { useState } from 'react';
+
+export function Password({ field }: { field: any }) {
+  const [show, setShow] = useState<boolean>(false);
+
+  return (
+    <InputGroup>
+      <InputGroupInput type={!show ? 'password' : 'text'} {...field} />
+      <InputGroupAddon>
+        <LockIcon />
+      </InputGroupAddon>
+      <InputGroupAddon align="inline-end">
+        <InputGroupButton
+          size="icon-xs"
+          onClick={() => setShow((prev) => !prev)}
+        >
+          {show ? <EyeIcon /> : <EyeClosedIcon />}
+        </InputGroupButton>
+      </InputGroupAddon>
+    </InputGroup>
+  );
+}
