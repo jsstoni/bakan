7d3bb33754d1069156d075decf4688a51ba845c4
jsstoni <jsstoniha@gmail.com>
2025-10-18 14:57:14 -0300

    feat(auth): change password

---
+++ app/dashboard/profile/_components/update-password.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import { Field } from '@/components/field';
+import { Password } from '@/components/password';
+import { Button } from '@/components/ui/button';
+import {
+  FieldDescription,
+  FieldGroup,
+  FieldLegend,
+  FieldSet,
+} from '@/components/ui/field';
+import { Form } from '@/components/ui/form';
+import { Spinner } from '@/components/ui/spinner';
+import { authClient } from '@/lib/auth/client';
+import { registerSchema } from '@/types/user';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+import z from 'zod';
+
+export function UpdatePassword() {
+  const passwordSchema = registerSchema
+    .omit({ email: true, name: true })
+    .extend({
+      currentpass: z.string(),
+    })
+    .refine((data) => data.password === data.repassword, {
+      path: ['repassword'],
+      message: 'Passwords do not match',
+    });
+
+  const form = useForm({
+    resolver: zodResolver(passwordSchema),
+    defaultValues: {
+      password: '',
+      repassword: '',
+      currentpass: '',
+    },
+  });
+
+  const onSubmit = form.handleSubmit(
+    async (values: z.infer<typeof passwordSchema>) => {
+      const { error } = await authClient.changePassword({
+        newPassword: values.password,
+        currentPassword: values.currentpass,
+        revokeOtherSessions: true,
+      });
+
+      if (error) {
+        return toast.error(error.message);
+      }
+
+      toast.success('Password changed successfully');
+    }
+  );
+
+  return (
+    <FieldSet>
+      <FieldLegend>Change your password</FieldLegend>
+      <Form {...form}>
+        <form className="max-w-xs space-y-4" onSubmit={onSubmit}>
+          <FieldGroup>
+            <Field
+              control={form.control}
+              name="currentpass"
+              label="Old Password"
+              render={(field) => <Password field={field} />}
+            />
+            <Field
+              control={form.control}
+              name="password"
+              label="New Password"
+              render={(field) => <Password field={field} />}
+            />
+            <Field
+              control={form.control}
+              name="repassword"
+              label="Confirm New Password"
+              render={(field) => <Password field={field} />}
+            />
+          </FieldGroup>
+
+          <Button type="submit">
+            {form.formState.isSubmitting && <Spinner />} Save
+          </Button>
+        </form>
+      </Form>
+    </FieldSet>
+  );
+}
--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,7 +1,11 @@
+import { UpdatePassword } from '@/app/dashboard/profile/_components/update-password';
+
 export default function Profile() {
   return (
-    <section>
-      <h1 className="container mx-auto py-5 font-bold text-3xl">Profile</h1>
+    <section className="container mx-auto">
+      <h1 className="py-5 font-bold text-3xl">Profile</h1>
+
+      <UpdatePassword />
     </section>
   );
 }
