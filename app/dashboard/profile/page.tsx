6aa7cfeb174c7afcf10f02937cc20d3fb666ba31
jsstoni <jsstoniha@gmail.com>
2025-10-28 16:32:16 -0300

    feat: add tabs

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,11 +1,24 @@
 import { UpdatePassword } from '@/app/dashboard/_components/update-password';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export default function Profile() {
   return (
-    <section className="container mx-auto">
-      <h1 className="py-5 font-bold text-3xl">Profile</h1>
-
-      <UpdatePassword />
+    <section className="container mx-auto py-5">
+      <Tabs defaultValue="account">
+        <div className="flex justify-between">
+          <h1 className="font-bold text-3xl">Profile</h1>
+          <TabsList>
+            <TabsTrigger value="account">Account</TabsTrigger>
+            <TabsTrigger value="password">Password</TabsTrigger>
+          </TabsList>
+        </div>
+        <TabsContent value="account">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="password">
+          <UpdatePassword />
+        </TabsContent>
+      </Tabs>
     </section>
   );
 }
