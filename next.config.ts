1aab39b16e03d596a8ab4159b69fc16398218059
jsstoni <jsstoniha@gmail.com>
2025-10-27 22:55:50 -0300

    ci: turbopack file system cache

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactCompiler: true,
   typedRoutes: true,
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
 };
 
 export default nextConfig;
