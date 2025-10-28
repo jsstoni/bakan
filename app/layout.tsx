01319f7f8dd2363887e629a6a931ff59984c38f1
jsstoni <jsstoniha@gmail.com>
2025-10-28 16:33:09 -0300

    refactor: connection is not necessary

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
 import { ThemeProvider } from '@/components/provider';
 import { Toaster } from '@/components/ui/sonner';
 import { Google } from '@/lib/analytics/client';
-import { connection } from 'next/server';
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -21,13 +20,11 @@ export const metadata: Metadata = {
   description: 'NEXT.js APP Router',
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  await connection();
-
   return (
     <html lang="en" suppressHydrationWarning>
       <body
